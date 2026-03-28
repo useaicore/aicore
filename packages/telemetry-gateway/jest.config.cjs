d76d1ab85f96f9d3dbdda3241efb26a407145753
Temitope <olalekanogunrekun@gmail.com>
2026-03-28 21:54:40 +0100

    feat: add telemetry gateway test suite and configuration

--- packages/telemetry-gateway/jest.config.cjs
+++ packages/telemetry-gateway/jest.config.cjs
@@ -3,6 +3,9 @@ module.exports = {
   transform: {
     "^.+\\.tsx?$": ["ts-jest", { useESM: true }]
   },
+  moduleNameMapper: {
+    "^(\\.{1,2}/.*)\\.js$": "$1"
+  },
   extensionsToTreatAsEsm: [".ts"],
   moduleFileExtensions: ["ts", "js", "json"],
   testMatch: ["**/__tests__/**/*.test.ts"],
